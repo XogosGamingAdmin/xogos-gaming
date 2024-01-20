232dea30af1b5ec9f2daa1871d78a4ea3c9cd70e
XogosGamingAdmin <143802740+XogosGamingAdmin@users.noreply.github.com>
2024-01-20 16:16:38 -0600

    Update checkout_1.php

--- stripe-one/checkout_1.php
+++ stripe-one/checkout_1.php
@@ -3,7 +3,7 @@
 include './stripe-php-master/init.php';
 //require 'vendor/autoload.php';
 // This is your test secret API key.
-\Stripe\Stripe::setApiKey('sk_test_51MFr5cJaC8AzBwqG3jOV3po07OT6UBwZvZsPFqmQeRatLDdb8YVlUoyhsPJKiOtEkScHARyM39GtTZNxwyPtDywV00Cc0rWW51');
+\Stripe\Stripe::setApiKey('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');
 
 header('Content-Type: application/json');
 
