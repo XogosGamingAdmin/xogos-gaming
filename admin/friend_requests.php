5b9b5cf705872959e76b2f00e04d6a14beb8f91d
Lukas Thern Lovén <103603662+thernloven@users.noreply.github.com>
2024-04-28 13:13:20 -0300

    friends and friend request

---
+++ admin/friend_requests.php
@@ -0,0 +1,66 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/sidebar.php" ?>
+<?php include "includes/navbar.php"; ?>
+
+<div class="content">
+<div class="row">
+          <div class="col-md-12">
+            <div class="card ">
+            <div class="card-header d-flex justify-content-between align-items-center">
+    <div> <!-- Flex container for vertical grouping of the title and search input -->
+        <h4 class="card-title">Friend Requests</h4>
+</div>
+</div>
+
+              <div class="card-body">
+                <div class="table-responsive">
+                <table class="table tablesorter">
+                        <thead class="text-primary">
+   
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td><img src="assets/img/avatars/avatar_4.png" alt="" style="width:50px; height:auto;"></td>
+                            <td>Lukas Thern Loven</td>
+                            <td class='text-right'><a href='LINK_TO_ADD_FRIEND'>Approve</a></td>
+                            <td class='text-right'><a href='LINK_TO_ADD_FRIEND'>Deny</a></td>
+
+                        </tr>
+                        <tr>
+                            <td><img src="assets/img/avatars/avatar_4.png" alt="" style="width:50px; height:auto;"></td>
+                            <td>Lukas Thern Loven</td>
+                            <td class='text-right'><a href='LINK_TO_ADD_FRIEND'>Approve</a></td>
+                            <td class='text-right'><a href='LINK_TO_ADD_FRIEND'>Deny</a></td>
+
+                        </tr>
+                        <tr>
+                            <td><img src="assets/img/avatars/avatar_4.png" alt="" style="width:50px; height:auto;"></td>
+                            <td>Lukas Thern Loven</td>
+                            <td class='text-right'><a href='LINK_TO_ADD_FRIEND'>Approve</a></td>
+                            <td class='text-right'><a href='LINK_TO_ADD_FRIEND'>Deny</a></td>
+
+                        </tr>
+                   </tbody>
+                   </table>
+
+                   <?php 
+                   
+                   if(isset($_GET['delete'])) {
+
+                    $client_id = $_GET['delete'];
+                    $query = "DELETE FROM users WHERE user_id = {$user_id}";
+                    $delete_query = mysqli_query($connection, $query);
+                     
+        update_kids_count();
+        update_kids_count_byteacher();
+                    header("Location: my_kids.php");
+
+
+                   }
+                   ?>
+
+                </div>
+              </div>
+            </div>
+          </div>
+</div>
