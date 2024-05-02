a5108729363636e33595564ffb13271516e9cd7a
thernloven <luke@live.se>
2024-05-02 21:44:42 +0700

    Add Request Friends Feature Fix

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php include "../admin/includes/db.php";
-
+session_start();
 $base_url = (isset($_SERVER['HTTPS'])
   && $_SERVER['HTTPS'] === 'on' ? "https" : "http")
   . "://" . $_SERVER['HTTP_HOST'];
